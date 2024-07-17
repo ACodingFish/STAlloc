3ff4c6c8380d6ae68ee3bf4c151c5f5dcf59ceb0
Jonathan <Jonmd@hotmail.com>
2024-07-17 12:50:44 -0500

    Added MIT License, fixes after Testing.
    lightly tested

--- stalloc.c
+++ stalloc.c
@@ -1,9 +1,32 @@
+/* *********************************************************************************
+MIT License
+
+Copyright (c) 2024 Jonathan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*********************************************************************************** */
+
 #include <string.h>
 #include <stdint.h>
 #include <stdio.h>
 
 #include "stalloc.h"
-
 
 
 typedef struct __attribute__((__packed__)) StallocEntry_t_def
@@ -13,12 +36,12 @@ typedef struct __attribute__((__packed__)) StallocEntry_t_def
 } StallocEntry_t;
 
 static StallocEntry_t st_registry[STALLOC_MAX_ALLOCS];  // Needs to be protected with a mutex
-uint32_t st_registry_count = 0;
+static uint32_t st_registry_count = 0;
 static uint8_t st_buf[STALLOC_MAX_BUF];     // Needs to be protected with a mutex
 
-uint8_t* stalloc_registry_find_space(uint32_t size);
-void stalloc_registry_allocate(uint8_t* p_start, uint32_t size);
-void stalloc_registry_free(uint8_t* p_start);
+static uint8_t* stalloc_registry_find_space(uint32_t size);
+static void stalloc_registry_allocate(uint8_t* p_start, uint32_t size);
+static void stalloc_registry_free(uint8_t* p_start);
 
 void Stalloc_Init(void)
 {
@@ -52,35 +75,37 @@ void Stfree(void* data)
 }
 
 
-uint8_t* stalloc_registry_find_space(uint32_t size)
+static uint8_t* stalloc_registry_find_space(uint32_t size)
 {
-    uint8_t *p_start=st_buf;
+    uint8_t *p_start=NULL;
+    uint8_t * next_p_start=st_buf;
     uint32_t buff_remaining=0;
 
-    for (uint32_t i = 0; i < st_registry_count; i++)
+    for (uint32_t i = 0; i < STALLOC_MAX_ALLOCS; i++)
     {  
         if (st_registry[i].end == 0) // Unregistered entry
         {
-            buff_remaining = (STALLOC_MAX_BUF - (uint32_t)(p_start-st_buf));
-            if (size < buff_remaining) // unregistered spot in registry and there is space
+            buff_remaining = ((uint32_t)STALLOC_MAX_BUF - (uint32_t)(next_p_start-st_buf));
+            if (size <= buff_remaining) // unregistered spot in registry and there is space
             {
-                // store
+                p_start=next_p_start; // store
             } else
             {
                 p_start=NULL;    // Nothing found
             }
             break; // Sorted array, so break regardless since we are at the end of known values
-        } else if ((st_registry[i].start - p_start) > size) // Registered entry, check against last entry
+        } else if ((st_registry[i].start - next_p_start) >= size) // Registered entry, check against last entry
         {
-            break; // store
+            p_start=next_p_start; // store
+            break;
         }
         
-        p_start=st_registry[i].end+1;
+        next_p_start=st_registry[i].end+1;
     }
     return p_start;
 }
 
-void stalloc_registry_allocate(uint8_t* p_start, uint32_t size)
+static void stalloc_registry_allocate(uint8_t* p_start, uint32_t size)
 {
     for (uint32_t i = 0; i <= st_registry_count; i++)
     {
@@ -97,14 +122,14 @@ void stalloc_registry_allocate(uint8_t* p_start, uint32_t size)
         if ((st_registry[i].start==0)||(st_registry[i].start > p_start)) // store
         {
             st_registry[i].start=p_start;
-            st_registry[i].end=p_start+size;
+            st_registry[i].end=p_start+size-1;
             st_registry_count++;
             break;
         }
     }
 }
 
-void stalloc_registry_free(uint8_t* p_start)
+static void stalloc_registry_free(uint8_t* p_start)
 {
     uint32_t index = 0;
 
@@ -119,7 +144,7 @@ void stalloc_registry_free(uint8_t* p_start)
             }
         }
 
-        memset(p_start, 0, (size_t)((uint8_t*)st_registry[index].end - p_start)); // Reset values to 0
+        memset((uint8_t*)st_registry[index].start, 0, (size_t)((uint8_t*)st_registry[index].end + 1 - (uint8_t*)st_registry[index].start)); // Reset values to 0
 
         for (uint32_t i = index; i < st_registry_count; i++)
         {
